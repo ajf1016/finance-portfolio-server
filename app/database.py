0b6e1f1278af9dc9f40c3f9621f8fd22f75ab575
ajf1016 <talropajmal1016@gmail.com>
2025-03-20 05:06:20 +0530

    db issue

--- app/database.py
+++ app/database.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 LIVE_DATABASE_URL = os.getenv(
-    "LIVE_DATABASE_URL", "postgresql://user:password@localhost/portfolio_db")
+    "LIVE_DATABASE_URL")
 
 engine = create_engine(LIVE_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
